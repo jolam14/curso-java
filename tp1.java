cc674f7d42a6852342ac6151c0f7f838e671a92b
jolam14 <127243794+jolam14@users.noreply.github.com>
2023-03-22 19:33:10 -0300

    Add files via upload

---
+++ tp1.java
@@ -0,0 +1,39 @@
+package clase;
+
+public class tp1 {
+
+	public static void main(String[] args) {
+		
+		int varA=5, varB=14;
+		while(varA<=varB) { 			 			
+			System.out.print("- "+varA); 			
+			varA=varA+1;
+			} 		
+		while(varA<=varB) { 			
+			if (varA%2 == 0) { 				
+			System.out.print("- "+varA);
+		while(varA<=varB) {
+			if (imprimirPares==true) {
+				if (varA%2 == 0) {
+					System.out.print("- "+varA); 				}
+			}
+		}
+			}else{ 				
+				if (varA%2 != 0) {
+					System.out.print("- "+varA); 				}
+				varA=varA+1; 		}
+			int numeroInicio = 5;
+			for(int numeroFinal=14; numeroFinal>=numeroInicio;numeroFinal--) {
+				System.out.println(numeroFinal);
+			}
+			if ( (ingreso<=163.539)&&(vehiculo2020<=0)&&(inmuebles<=1)) {
+				System.out.println("categoria 1");}
+			else if ((ingreso<=572.386)&&(vehiculo2020<=1)&&(inmueble<=2)) {
+			System.out.println("categoria 2");
+			}
+			else {
+				System.out.println("categoria 3");}
+				
+	}				
+}
+}
